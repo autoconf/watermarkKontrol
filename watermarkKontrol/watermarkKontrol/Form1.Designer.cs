45ecfaadfd12253859221572521f426e269a98ac
ceburakyilmaz <burakyilmaztr55@gmail.com>
2020-05-19 03:30:31 +0300

    rgb, r, g, b, bantlardan metin çıkarma yapıldı

--- watermarkKontrol/watermarkKontrol/Form1.Designer.cs
+++ watermarkKontrol/watermarkKontrol/Form1.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metniAyirButon = new System.Windows.Forms.PictureBox();
+            this.goruntuSecPictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.metinRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.metniAyirButon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goruntuSecPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metniAyirButon
+            // 
+            this.metniAyirButon.Image = ((System.Drawing.Image)(resources.GetObject("metniAyirButon.Image")));
+            this.metniAyirButon.Location = new System.Drawing.Point(61, 311);
+            this.metniAyirButon.Margin = new System.Windows.Forms.Padding(4);
+            this.metniAyirButon.Name = "metniAyirButon";
+            this.metniAyirButon.Size = new System.Drawing.Size(399, 125);
+            this.metniAyirButon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.metniAyirButon.TabIndex = 4;
+            this.metniAyirButon.TabStop = false;
+            this.metniAyirButon.Click += new System.EventHandler(this.metniAyirButon_Click);
+            // 
+            // goruntuSecPictureBox
+            // 
+            this.goruntuSecPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("goruntuSecPictureBox.Image")));
+            this.goruntuSecPictureBox.Location = new System.Drawing.Point(13, 13);
+            this.goruntuSecPictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.goruntuSecPictureBox.Name = "goruntuSecPictureBox";
+            this.goruntuSecPictureBox.Size = new System.Drawing.Size(515, 290);
+            this.goruntuSecPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.goruntuSecPictureBox.TabIndex = 3;
+            this.goruntuSecPictureBox.TabStop = false;
+            this.goruntuSecPictureBox.Click += new System.EventHandler(this.goruntuSecPictureBox_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(977, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(39, 39);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 56;
+            this.pictureBox2.TabStop = false;
+            // 
+            // metinRichTextBox
+            // 
+            this.metinRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.metinRichTextBox.Location = new System.Drawing.Point(552, 32);
+            this.metinRichTextBox.Name = "metinRichTextBox";
+            this.metinRichTextBox.ReadOnly = true;
+            this.metinRichTextBox.Size = new System.Drawing.Size(382, 390);
+            this.metinRichTextBox.TabIndex = 55;
+            this.metinRichTextBox.Text = "Metin burada gösterilecektir.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(536, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 423);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 54;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1028, 444);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.metinRichTextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metniAyirButon);
+            this.Controls.Add(this.goruntuSecPictureBox);
+            this.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Su İzi (watermark) Kontrol";
+            ((System.ComponentModel.ISupportInitialize)(this.metniAyirButon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goruntuSecPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox metniAyirButon;
+        private System.Windows.Forms.PictureBox goruntuSecPictureBox;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RichTextBox metinRichTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- watermarkKontrol/watermarkKontrol/Form1.cs
+++ watermarkKontrol/watermarkKontrol/Form1.cs
@@ -16,5 +16,214 @@ namespace watermarkKontrol
         {
             InitializeComponent();
         }
+        int uzunluk = 0;
+        private void goruntuSecPictureBox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Image Files(*.bmp)|*.bmp";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    goruntuSecPictureBox.Image = new Bitmap(open.FileName);
+                    if (goruntuSecPictureBox.Image.Height < goruntuSecPictureBox.Height || goruntuSecPictureBox.Image.Width < goruntuSecPictureBox.Width)
+                    {
+                        goruntuSecPictureBox.SizeMode = PictureBoxSizeMode.CenterImage;
+                        /// görüntü içeriği okunacak
+                        gorselOku(open.SafeFileName);
+
+                    }
+                    else
+                    {
+                        goruntuSecPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Görüntü Seçiminde Hata");
+            }
+        }
+
+        private void gorselOku(string dosyaadi)
+        {
+            char[] silinecek = { '.', 'b', 'm', 'p', ' ' };
+            uzunluk = Convert.ToInt32(dosyaadi.TrimEnd(silinecek));
+            Bitmap gorsel = new Bitmap(goruntuSecPictureBox.Image);
+        }
+
+        private void metniAyirButon_Click(object sender, EventArgs e)
+        {
+            Bitmap gorsel = new Bitmap(goruntuSecPictureBox.Image);
+
+            string ilk6piksel = string.Empty;
+            for (int x = 0; x < 6; x++)
+            {
+                Color piksel = gorsel.GetPixel(x, 0);
+                ilk6piksel += Convert.ToString((int)piksel.B, 2).Last();
+            }
+
+
+            ///Koda bağlı binary metin okuma işlemleri 
+            ///
+            string metin = string.Empty;
+
+            //ilk 6 bitten sonra renk paletinden metin çıkarılıyor..
+            if (ilk6piksel[0] == '0' && ilk6piksel[1] == '0')
+            {
+                //şifreleme yok
+                if (ilk6piksel[2] == '1' && ilk6piksel[3] == '0')
+                {
+                    //tüm piksel
+                    if (ilk6piksel[4] == '0' && ilk6piksel[5] == '0')
+                    {
+                        //tüm palet
+                        int i = 0;
+                        string alpha = string.Empty;
+                        for (int y = 0; y < gorsel.Height; y++) //gorsel.Height
+                        {
+                            for (int x = 0; x < gorsel.Width; x++) //gorsel.Width
+                            {
+                                if (y == 0 && x == 0)
+                                {
+                                    x = x + 7;
+                                }
+                                if (i < uzunluk - 1)
+                                {
+                                    Color piksel = gorsel.GetPixel(x, y);
+                                    string binaryA = string.Empty;
+                                    string binaryR = string.Empty;
+                                    string binaryG = string.Empty;
+                                    string binaryB = string.Empty;
+                                    binaryA = Convert.ToString((int)piksel.A, 2);
+                                    binaryR = Convert.ToString((int)piksel.R, 2);
+                                    binaryG = Convert.ToString((int)piksel.G, 2);
+                                    binaryB = Convert.ToString((int)piksel.B, 2);
+                                    metin += binaryR.Last().ToString() + binaryG.Last().ToString() + binaryB.Last().ToString();
+                                    i += 3;
+                                }
+                            }
+                        }
+                        metniMetinKutusunaYaz(metin);
+                    }
+                    else if (ilk6piksel[4] == '0' && ilk6piksel[5] == '1')
+                    {
+                        //kirmizi palet
+                        int i = 0;
+                        string alpha = string.Empty;
+                        for (int y = 0; y < gorsel.Height; y++) //gorsel.Height
+                        {
+                            for (int x = 0; x < gorsel.Width; x++) //gorsel.Width
+                            {
+                                if (y == 0 && x == 0)
+                                {
+                                    x = x + 7;
+                                }
+                                if (i < uzunluk)
+                                {
+                                    Color piksel = gorsel.GetPixel(x, y);
+                                    string binaryR = string.Empty;
+                                    binaryR = Convert.ToString((int)piksel.R, 2);
+                                    metin += binaryR.Last().ToString();
+                                    i += 1;
+                                }
+                            }
+                        }
+                        metniMetinKutusunaYaz(metin);
+                    }
+                    else if (ilk6piksel[4] == '1' && ilk6piksel[5] == '0')
+                    {
+                        //yesil palet
+                        int i = 0;
+                        string alpha = string.Empty;
+                        for (int y = 0; y < gorsel.Height; y++) //gorsel.Height
+                        {
+                            for (int x = 0; x < gorsel.Width; x++) //gorsel.Width
+                            {
+                                if (y == 0 && x == 0)
+                                {
+                                    x = x + 7;
+                                }
+                                if (i < uzunluk)
+                                {
+                                    Color piksel = gorsel.GetPixel(x, y);
+                                    string binaryG = string.Empty;
+                                    binaryG = Convert.ToString((int)piksel.G, 2);
+                                    metin += binaryG.Last().ToString();
+                                    i += 1;
+                                }
+                            }
+                        }
+                        metniMetinKutusunaYaz(metin);
+                    }
+                    else if (ilk6piksel[4] == '1' && ilk6piksel[5] == '1')
+                    {
+                        //mavi palet
+                        int i = 0;
+                        string alpha = string.Empty;
+                        for (int y = 0; y < gorsel.Height; y++) //gorsel.Height
+                        {
+                            for (int x = 0; x < gorsel.Width; x++) //gorsel.Width
+                            {
+                                if (y == 0 && x == 0)
+                                {
+                                    x = x + 7;
+                                }
+                                if (i < uzunluk)
+                                {
+                                    Color piksel = gorsel.GetPixel(x, y);
+                                    string binaryB = string.Empty;
+                                    binaryB = Convert.ToString((int)piksel.B, 2);
+                                    metin += binaryB.Last().ToString();
+                                    i += 1;
+                                }
+                            }
+                        }
+                        metniMetinKutusunaYaz(metin);
+                    }
+                }
+                else if (ilk6piksel[2] == '0' && ilk6piksel[3] == '0')
+                {
+                    //tek piksel
+                }
+                else if (ilk6piksel[2] == '0' && ilk6piksel[3] == '1')
+                {
+                    //çift piksel
+                }
+            }
+            else if (ilk6piksel[0] == '1' && ilk6piksel[1] == '0')
+            {
+                //reverse ascii
+            }
+            else if (ilk6piksel[0] == '1' && ilk6piksel[1] == '1')
+            {
+                //steganografi
+            }
+        }
+        private void metniMetinKutusunaYaz(string metin)
+        {
+            try
+            {
+                if (metin.Length > uzunluk)
+                {
+                    metin = metin.Substring(0, uzunluk);
+                }
+                //MessageBox.Show(metin + "\n" + metin.Length);
+                int fark = metin.Length - uzunluk;
+                //metne çevir
+                List<Byte> byteList = new List<Byte>();
+                //8den başlıyor --> 8bit-1byte
+                for (int x = 0; x < metin.Length - fark; x += 8)
+                {
+                    byteList.Add(Convert.ToByte(metin.Substring(x, 8), 2));
+                }
+                metinRichTextBox.Text = Encoding.ASCII.GetString(byteList.ToArray());
+            }
+            catch
+            {
+                MessageBox.Show($"Metin okuma hatası {metin.Length} karakter çevirilemiyor.");
+            }
+            
+        }
     }
 }
